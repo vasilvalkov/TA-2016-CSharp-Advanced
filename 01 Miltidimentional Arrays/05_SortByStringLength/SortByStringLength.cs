96543e62e5fdb6c1349044d4321564ee8a56c354
vasilvalkov <vasil.valkov@gmail.com>
2016-11-14 18:46:31 +0200

    Sort By String Length -  solved

---
+++ 01 Miltidimentional Arrays/05_SortByStringLength/SortByStringLength.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class SortByStringLength
+{
+    static void Main()
+    {
+        // Initialize input and variables
+        string input = Console.ReadLine();
+        string[] arr = input.Split(' ').ToArray();
+        // Sort array by string length
+        Array.Sort(arr, (x, y) => x.Length.CompareTo(y.Length));        
+        // Print sorted array
+        foreach (string str in arr)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
