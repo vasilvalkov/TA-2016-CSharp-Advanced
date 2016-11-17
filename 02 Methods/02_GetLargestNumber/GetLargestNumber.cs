0315dd7b2124ea7cfbbad0b9ecb8ef394780f049
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 15:51:20 +0200

    Get Largest Number - solved

---
+++ 02 Methods/02_GetLargestNumber/GetLargestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+class GetLargestNumber
+{
+    static int GetMax(int firstNumber, int secondNumber)
+    {
+        if (firstNumber > secondNumber)
+            return firstNumber;
+        else
+            return secondNumber;
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int[] inputArr = input.Split(' ').Select(int.Parse).ToArray();
+        int max = int.MinValue;
+
+        for (int i = 0; i < inputArr.Length - 1; i++)
+        {
+            int currentMax = GetMax(inputArr[i], inputArr[i + 1]);
+
+            if (max < currentMax)
+            {
+                max = currentMax;
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
