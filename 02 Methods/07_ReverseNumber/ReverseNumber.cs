9a0e796a8e1541fd6201d8271907608d41e37d4f
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 19:01:30 +0200

    Reverse Number - solved

---
+++ 02 Methods/07_ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ReverseNumber
+{
+    static decimal Reverse(decimal number)
+    {
+        string reversed = string.Empty;
+
+        for (int i = number.ToString().Length - 1; i >= 0 ; i--)
+        {
+            reversed += number.ToString()[i];
+        }
+
+        return decimal.Parse(reversed);
+    }
+
+    static void Main()
+    {
+        decimal input = decimal.Parse(Console.ReadLine());
+
+        decimal reversed = Reverse(input);
+
+        Console.WriteLine(reversed);
+    }
+}
