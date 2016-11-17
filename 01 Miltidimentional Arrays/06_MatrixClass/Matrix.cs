1468be360266ecd97be37bb16017f5af4a0f489f
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 15:05:05 +0200

    Matrix Class - solved

---
+++ 01 Miltidimentional Arrays/06_MatrixClass/Matrix.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] CreateNew(int rows, int cols)
+    {
+        return new int[rows, cols];
+    }
+    public static int[,] Add(int[,] firstMatrix, int[,] secondMatrix)
+    {
+        int[,] thirdMatrix = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+
+        for (int row = 0; row < firstMatrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < firstMatrix.GetLength(1); col++)
+            {
+                thirdMatrix[row, col] = firstMatrix[row, col] + secondMatrix[row, col];
+            }
+        }
+
+        return thirdMatrix;
+    }
+    public static int[,] Subtract(int[,] firstMatrix, int[,] secondMatrix)
+    {
+        int[,] thirdMatrix = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+
+        for (int row = 0; row < firstMatrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < firstMatrix.GetLength(1); col++)
+            {
+                thirdMatrix[row, col] = firstMatrix[row, col] - secondMatrix[row, col];
+            }
+        }
+
+        return thirdMatrix;
+    }
+    public static int[,] Product(int[,] firstMatrix, int[,] secondMatrix)
+    {
+        int[,] thirdMatrix = new int[secondMatrix.GetLength(0), secondMatrix.GetLength(1)];
+
+        for (int row = 0; row < firstMatrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < secondMatrix.GetLength(1); col++)
+            {
+                for (int i = 0; i < firstMatrix.GetLength(0); i++)
+                {
+                    thirdMatrix[row, col] += firstMatrix[row, i] * secondMatrix[i, col];
+                }
+            }
+        }
+
+        return thirdMatrix;
+    }
+    public static void Print(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write("{0} ", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[,] m1 = {
+                { 2, 2, 2 },
+                { 3, 3, 3 },
+                { 2, 4, 6 }
+            };
+
+        int[,] m2 = {
+                { 2, 3, 4 },
+                { 3, 5, 7 },
+                { 1, 3, 5 }
+            };
+
+        int[,] m3 = Matrix.Add(m1, m2);
+        int[,] m4 = Matrix.Subtract(m1, m2);
+        int[,] m5 = Matrix.Product(m1, m2);
+
+        Matrix.Print(m3);
+        Console.WriteLine();
+        Matrix.Print(m4);
+        Console.WriteLine();
+        Matrix.Print(m5);
+    }
+}
--- 01 Miltidimentional Arrays/07_LargestAreaInMatrix/LargestAreaInMatrix.cs
+++ 01 Miltidimentional Arrays/07_LargestAreaInMatrix/LargestAreaInMatrix.cs
@@ -1,10 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 class LargestAreaInMatrix
 {
+    static int IncreaseMaxLen(int current, int max)
+    {
+        if (max < current)
+        {   // Increment maximal found sequence
+            max = current;
+        }
+        return max;
+    }
+    
     static void Main()
     {
+        // INITIALIZE INPUT AND VARIABLES
+        string input = Console.ReadLine();
+        int[] dimSizes = input.Split(' ').Select(int.Parse).ToArray(); // [0] = rows, [1] = cols
+        int[,] matrix = new int[dimSizes[0], dimSizes[1]];
+        for (int row = 0; row < dimSizes[0]; row++)
+        {
+            int[] matrixRow = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < dimSizes[1]; i++)
+            {
+                matrix[row, i] = matrixRow[i];
+            }
+        }
+        int maxPathLen = 1;
 
+        // FIND LARGEST AREA OF EQUAL ELEMENTS
+        for (int r = 0; r < dimSizes[0]; r++)
+        {
+            for (int c = 0; c < dimSizes[1]; c++)
+            {
+                int currentValue = matrix[r, c];
+                int matchValueRow = r;
+                int matchValueCol = c;
+                int pathLength = 0;
+
+                if (matrix[i, j] == currentValue &&
+                    Math.Abs(matchValueRow - i) < 2 &&
+                    Math.Abs(matchValueCol - j) < 2)
+                {
+                    pathLength++;
+                    matchValueRow = i;
+                    matchValueCol = j;
+                    maxPathLen = IncreaseMaxLen(pathLength, maxPathLen);
+                }
+            }
+        }
+        // Print result
+        Console.WriteLine(maxPathLen);
     }
 }
