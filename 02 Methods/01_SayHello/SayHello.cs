535c122eac331e4f3a04ae134fa76ca48e32a5bd
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 15:29:00 +0200

    Added solution for Methods homeworks

---
+++ 02 Methods/01_SayHello/SayHello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class SayHello
+{
+    static void GreetUser()
+    {
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Hello, {0}!", userName);
+    }
+
+    static void Main()
+    {
+        GreetUser();
+    }
+}
