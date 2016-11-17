742151590bd5faa23bb04811391612e1ca1ca9e2
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 17:45:15 +0200

    First Larger Than Neiughbors - solved

---
+++ 02 Methods/06_FirstLargerThanNeighbours/FirstLargerThanNeiughbours.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class FirstLargerThanNeiughbours
+{
+    static int FirstLargerThanNeighbors(int[] numbers)
+    {
+        for (int i = 1; i < numbers.Length - 1; i++)
+        {
+            if ((numbers[i] > numbers[i - 1]) && (numbers[i] > numbers[i + 1]))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    static void Main()
+    {
+        int elemCount = int.Parse(Console.ReadLine());
+        string inputArr = Console.ReadLine();
+        int[] arr = inputArr.Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine(FirstLargerThanNeighbors(arr));
+    }
+}
