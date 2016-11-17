e3919b77d03fe88656320ed3ed4fca3c3487f0e9
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 16:49:16 +0200

    Appearance Count - solved

---
+++ 02 Methods/04_AppearanceCount/AppearanceCount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class AppearanceCount
+{
+    static int TimesAppear(int[] numbers, int target)
+    {
+        int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] == target)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        int arrSize = int.Parse(Console.ReadLine());
+        string arrNumbers = Console.ReadLine();
+        int[] arr = arrNumbers.Split(' ').Select(int.Parse).ToArray();
+        int target = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(TimesAppear(arr, target));
+    }
+}
