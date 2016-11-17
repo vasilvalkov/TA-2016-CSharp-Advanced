dbfb626b4317a7247e4803c5c144a00e5bd81984
vasilvalkov <vasil.valkov@gmail.com>
2016-11-17 17:37:18 +0200

    Larger Than Neighbors - solved

---
+++ 02 Methods/05_LargerThanNeighbours/LargerThanNeighbours.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class LargerThanNeighbours
+{
+    static int LargerThanNeigboursCount(int[] numbers)
+    {
+        int count = 0;
+
+        for (int i = 1; i < numbers.Length - 1; i++)
+        {
+            if ((numbers[i] > numbers[i - 1]) && (numbers[i] > numbers[i + 1]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        int elemCount = int.Parse(Console.ReadLine());
+        string inputArr = Console.ReadLine();
+        int[] arr = inputArr.Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine(LargerThanNeigboursCount(arr));
+    }
+}
