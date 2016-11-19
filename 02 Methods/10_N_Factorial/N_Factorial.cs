d9b13c449d3b86ad3d9ff82a6e7d9d092547e4aa
vasilvalkov <vasil.valkov@gmail.com>
2016-11-19 10:02:18 +0200

    N Factorial - solved

---
+++ 02 Methods/10_N_Factorial/N_Factorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+class N_Factorial
+{
+    static BigInteger Factorial(char[] numbers)
+    {
+        string numberStr = string.Empty;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numberStr += numbers[i];
+        }
+
+        BigInteger number = BigInteger.Parse(numberStr);
+        BigInteger factorial = 1;
+
+        for (BigInteger i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Factorial(Console.ReadLine().ToArray()));
+    }
+}
