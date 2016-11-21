f1ab48ce5f3ff3849314fab0d7d41682470054e9
vasilvalkov <vasil.valkov@gmail.com>
2016-11-21 14:46:35 +0200

    Hexadecimal To Binary - solved

---
+++ 03 Numeral Systems/05_HexadecimalToBinary/HexToBinary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class HexToBinary
+{
+    static ulong HexToDec(string number)
+    {
+        ulong decValue = 0;
+        int value = 0;
+
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                value = digit - '0';
+            }
+            else
+            {
+                value = digit - 'A' + 10;
+            }
+
+            decValue = decValue * 16 + (ulong)value;
+        }
+
+        return decValue;
+    }
+
+    static string HexToBin(string hex)
+    {
+        string bin = string.Empty;
+
+        ulong dec = HexToDec(hex);
+
+        while (dec > 0)
+        {
+            bin = (dec % 2).ToString() + bin;
+            dec >>= 1;
+        }
+
+        return bin;
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        Console.WriteLine(HexToBin(input));
+    }
+}
