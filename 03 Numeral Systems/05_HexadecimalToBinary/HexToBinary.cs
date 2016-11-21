a0c23bf74e92a8a8d03bbf980fc26d6d2515a1e6
vasilvalkov <vasil.valkov@gmail.com>
2016-11-21 16:12:27 +0200

    Hexadecimal To Binary - solved with direct convertion

--- 03 Numeral Systems/05_HexadecimalToBinary/HexToBinary.cs
+++ 03 Numeral Systems/05_HexadecimalToBinary/HexToBinary.cs
@@ -1,48 +1,44 @@
 ﻿using System;
 
 class HexToBinary
-{
-    static ulong HexToDec(string number)
-    {
-        ulong decValue = 0;
-        int value = 0;
-
-        foreach (char digit in number)
-        {
-            if (char.IsDigit(digit))
-            {
-                value = digit - '0';
-            }
-            else
-            {
-                value = digit - 'A' + 10;
-            }
-
-            decValue = decValue * 16 + (ulong)value;
-        }
-
-        return decValue;
-    }
-
+{  
     static string HexToBin(string hex)
     {
         string bin = string.Empty;
 
-        ulong dec = HexToDec(hex);
-
-        while (dec > 0)
+        foreach (char digit in hex)
         {
-            bin = (dec % 2).ToString() + bin;
-            dec >>= 1;
-        }
+            switch (digit)
+            {
+                case '0': bin += "0000";  break;
+                case '1': bin += "0001";  break;
+                case '2': bin += "0010";  break;
+                case '3': bin += "0011";  break;
+                case '4': bin += "0100";  break;
+                case '5': bin += "0101";  break;
+                case '6': bin += "0110";  break;
+                case '7': bin += "0111";  break;
+                case '8': bin += "1000";  break;
+                case '9': bin += "1001";  break;
+                case 'A': bin += "1010";  break;
+                case 'B': bin += "1011";  break;
+                case 'C': bin += "1100";  break;
+                case 'D': bin += "1101";  break;
+                case 'E': bin += "1110";  break;
+                case 'F': bin += "1111";  break;
+            }
 
-        return bin;
+            if (bin.Length == 4)
+            {
+                bin = bin.TrimStart('0');
+            }
+        }
+        return bin == string.Empty ? "0" : bin;
     }
 
     static void Main()
     {
         string input = Console.ReadLine();
-
         Console.WriteLine(HexToBin(input));
     }
 }
