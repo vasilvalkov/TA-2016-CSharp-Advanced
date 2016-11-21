6c1edf879e1760dc15de2b5377e93848124fc59d
vasilvalkov <vasil.valkov@gmail.com>
2016-11-21 11:54:52 +0200

    Binary To Decimal - solved

---
+++ 03 Numeral Systems/02_BinaryToDecimal/BinaryToDecimal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class BinaryToDecmimal
+{
+    static ulong BinToDec(string bin)
+    {
+        ulong dec = 0;
+        int pow = 0;
+
+        for (int i = bin.Length - 1; i >= 0; i--)
+        {
+            dec += (ulong)(bin[i] - '0') * (1UL << pow++);
+        }
+
+        return dec;
+    }
+
+    static void Main()
+    {
+        string binNumber = Console.ReadLine();
+
+        Console.WriteLine(BinToDec(binNumber));
+    }
+}
