867815a632606f54420dddf528b9b69cbc488433
vasilvalkov <vasil.valkov@gmail.com>
2016-11-21 11:10:36 +0200

    Decimal To Binary - solved

--- 03 Numeral Systems/01_DecimalToBinary/DecimalToBinary.cs
+++ 03 Numeral Systems/01_DecimalToBinary/DecimalToBinary.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class DecimalToBinary
 {
+    static long decNumber;
+
+    static void PrepareInput(string s)
+    {
+        decNumber = long.Parse(s);
+    }
+
+    static string DecToBinary(long number)
+    {
+        string bin = string.Empty;
+
+        while (number > 0)
+        {
+            bin = (number % 2).ToString() + bin;
+            number >>= 1;
+        }
+
+        return bin;
+    }
+
     static void Main()
     {
-
+        PrepareInput(Console.ReadLine());
+        Console.WriteLine(DecToBinary(decNumber));
     }
 }
