f4cf2fefe3caa15d8f08087f611431a7f44682bd
vasilvalkov <vasil.valkov@gmail.com>
2016-11-24 15:31:08 +0200

    Random Numbers - solved

---
+++ 04 Using Classes and Objects/02_RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02_RandomNumbers
+{
+    class RandomNumbers
+    {
+        static Random rand = new Random();
+
+        static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(rand.Next(100, 200));
+            }
+            
+        }
+    }
+}
