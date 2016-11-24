d0df9e13d45200044862aad446fe4d95ab271312
vasilvalkov <vasil.valkov@gmail.com>
2016-11-24 18:05:35 +0200

    Triangle Surface By Two Sides And An Angle - solved

---
+++ 04 Using Classes and Objects/06_TriangleSurfaceByTwo SidesAndAnAngle/TriangleSurfaceByTwoSidesAndAnAngle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06_TriangleSurfaceByTwo_SidesAndAnAngle
+{
+    static class Triangle
+    {
+        public static double AreaByTwoSidesAndAngle(double side1, double side2, double angle)
+        {
+            double area;
+            area = ((side1 * side2 * Math.Sin(angle * Math.PI / 180)) / 2) ;
+            return area;
+        }
+    }
+
+    class TriangleSurfaceByTwoSidesAndAnAngle
+    {
+        static void Main()
+        {
+            double side1 = double.Parse(Console.ReadLine());
+            double side2 = double.Parse(Console.ReadLine());
+            double angle = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:F2}", Triangle.AreaByTwoSidesAndAngle(side1, side2, angle));
+
+        }
+    }
+}
