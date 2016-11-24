40beb72994249c34eb8e7835a1a9936f290f7c79
vasilvalkov <vasil.valkov@gmail.com>
2016-11-24 18:29:28 +0200

    Sum Integers - solved

---
+++ 04 Using Classes and Objects/07_Workdays/Workdays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace _07_Workdays
+{
+    class Workdays
+    {
+        static int CountWorkdaysTo(DateTime date)
+        {
+            int days = 0;
+
+            DateTime.Today;
+
+            return days;
+        }
+
+        static void Main()
+        {
+            DateTime input = DateTime.ParseExact(string.Format("{0:dd.MM.yyyy}",Console.ReadLine()), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(CountWorkdaysTo(input));
+        }
+    }
+}
---
+++ 04 Using Classes and Objects/08_SumIntegers/SumIntegers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _09_SumIntegers
+{
+    class SumIntegers
+    {
+        static int PrepareInput(string input)
+        {
+            int intSum = input.Split(' ')
+                              .Select(int.Parse)
+                              .ToArray()
+                              .Sum();
+
+            return intSum;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(  PrepareInput(Console.ReadLine()));
+        }
+    }
+}
