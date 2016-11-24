79ed6c591fadfe5477f41f0238dcc678f728771d
vasilvalkov <vasil.valkov@gmail.com>
2016-11-24 15:50:21 +0200

    Triangle Surface By Side And Altitude - solved

---
+++ 04 Using Classes and Objects/04_TriangleSurfaceBySideAndAltitude/TriangleSurfaceBySideAndAltitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_TriangleSurfaceBySideAndAltitude
+{
+    static class Triangle
+    {
+        public static double AreaBySideAndAlt(double side, double altitude)
+        {
+            double area = (side * altitude) / 2;
+            return area;
+        }
+    }
+
+    class TriangleSurfaceBySideAndAltitude
+    {
+        static void Main()
+        {
+            double side = double.Parse(Console.ReadLine());
+            double alt = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:F2}" ,Triangle.AreaBySideAndAlt(side, alt));
+        }
+    }
+}
