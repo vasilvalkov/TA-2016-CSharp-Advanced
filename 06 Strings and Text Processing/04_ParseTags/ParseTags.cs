e27df7f5babdae0816f4431de13cf7824f2f7257
vasilvalkov <vasil.valkov@gmail.com>
2016-11-25 15:01:41 +0200

    Parse Tags - solved

---
+++ 06 Strings and Text Processing/04_ParseTags/ParseTags.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace _04_ParseTags
+{
+    class ParseTags
+    {
+        static string openingTag = "<upcase>";
+        static string closingTag = "</upcase>";
+
+        static void Main()
+        {
+            string text = Console.ReadLine();            
+
+            Console.WriteLine(ParseText(text));
+        }
+
+        private static string ParseText(string text)
+        {
+            StringBuilder parsedText = new StringBuilder();
+            parsedText.Append(text);
+
+            int index = 0;
+
+            while (0 <= index && index < text.Length)
+            {
+                int currentOpenIndex = text.IndexOf(openingTag, index);
+                int currentCloseIndex = text.IndexOf(closingTag, index);
+
+                if (currentOpenIndex < 0 || index >= text.Length)
+                    break;
+
+                for (int i = currentOpenIndex + openingTag.Length; i < currentCloseIndex; i++)
+                {
+                    parsedText[i] = char.ToUpper(text[i]);
+                }
+                index = currentCloseIndex + closingTag.Length;
+            }
+
+            parsedText = parsedText.Replace(openingTag, string.Empty);
+            parsedText = parsedText.Replace(closingTag, string.Empty);
+            return parsedText.ToString();
+        }
+    }
+}
