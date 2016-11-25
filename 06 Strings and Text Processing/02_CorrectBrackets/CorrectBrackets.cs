c154028193a027854c3e46b3b457537ff685502f
vasilvalkov <vasil.valkov@gmail.com>
2016-11-25 12:44:11 +0200

    Correct Brackets - solved

---
+++ 06 Strings and Text Processing/02_CorrectBrackets/CorrectBrackets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02_CorrectBrackets
+{
+    class CorrectBrackets
+    {
+        static string AreCorrect(string text)
+        {
+            int openingBrackets = 0;
+            int closingBrackets = 0;
+
+            foreach (var symbol in text)
+            {
+                if (symbol == '(')
+                {
+                    openingBrackets++;
+                }
+                if (symbol == ')')
+                {
+                    closingBrackets++;
+                }
+            }
+
+            return openingBrackets == closingBrackets ? "Correct" : "Incorrect";
+        }
+
+        static void Main()
+        {
+            string brackets = Console.ReadLine();
+
+            Console.WriteLine(AreCorrect(brackets));
+        }
+    }
+}
