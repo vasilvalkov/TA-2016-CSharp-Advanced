87bb09b4c0a24bd7668c854e0669a49224fdaaad
vasilvalkov <vasil.valkov@gmail.com>
2016-11-25 14:04:29 +0200

    Sub-string In Text - solved

---
+++ 06 Strings and Text Processing/03_SubstringInText/SubstringInText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03_SubstringInText
+{
+    class SubstringInText
+    {
+        static int PatternCount(string pattern, string text)
+        {
+            int count = 0;
+            int index = 0;
+
+            while (0 <= index && index < text.Length)
+            {
+                index = text.IndexOf(pattern, index, StringComparison.OrdinalIgnoreCase);
+
+                if (!(index < 0))
+                {
+                    count++;
+                    index += pattern.Length;
+                }                
+            }
+
+            return count;
+        }
+
+        static void Main()
+        {
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            Console.WriteLine(PatternCount(pattern, text));
+        }
+    }
+}
