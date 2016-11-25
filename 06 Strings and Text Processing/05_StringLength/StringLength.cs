2716520d01101ab9db5bb33b19129466ca082f6c
vasilvalkov <vasil.valkov@gmail.com>
2016-11-25 15:36:19 +0200

    String Length - solved

---
+++ 06 Strings and Text Processing/05_StringLength/StringLength.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_StringLength
+{
+    class StringLength
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(CheckLength(input));
+        }
+
+        static string CheckLength(string text)
+        {
+            if (text.Length <= 20)
+            {
+                text = string.Concat(text, new string('*', 20 - text.Length));
+            }
+            return text;
+        }
+    }
+}
