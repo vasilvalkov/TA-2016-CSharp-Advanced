c8824a53ba0918100c2404fc117319b0194716ca
vasilvalkov <vasil.valkov@gmail.com>
2016-11-27 13:35:44 +0200

    Unicode Characters - solved

---
+++ 06 Strings and Text Processing/09_UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace _09_UnicodeCharacters
+{
+    class UnicodeCharacters
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(ToUnicode(input));
+        }
+
+        private static string ToUnicode(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                sb.AppendFormat("{0}{1:X4}", "\\u", (int)text[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
