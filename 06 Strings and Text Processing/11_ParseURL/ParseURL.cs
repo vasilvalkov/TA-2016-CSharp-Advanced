770eee49549781d7b7cd4d225d5f1f3db27ef935
vasilvalkov <vasil.valkov@gmail.com>
2016-11-27 14:22:38 +0200

    Parse URL - solved

---
+++ 06 Strings and Text Processing/11_ParseURL/ParseURL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _11_ParseURL
+{
+    class ParseURL
+    {
+        static void Main()
+        {
+            string url = Console.ReadLine();
+            Console.WriteLine(Parse(url));
+        }
+
+        private static string Parse(string url)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[protocol] = ");
+            sb.Append(url, 0, url.IndexOf(':'));
+
+            sb.AppendLine();
+            sb.Append("[server] = ");
+            sb.Append(url,
+                      url.IndexOf('/') + 2,
+                      url.IndexOf('/', url.IndexOf('.')) - (url.IndexOf('/') + 2));
+
+            sb.AppendLine();
+            sb.Append("[resource] = ");
+            sb.Append(url,
+                      url.IndexOf('/', url.IndexOf('.')),
+                      url.Length - url.IndexOf('/', url.IndexOf('.')));
+
+            return sb.ToString();
+        }
+    }
+}
