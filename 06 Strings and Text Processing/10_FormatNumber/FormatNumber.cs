920e5d10b80fc1447969e0be27d85f2cdb9aedf1
vasilvalkov <vasil.valkov@gmail.com>
2016-11-29 08:52:08 +0200

    Format Number - solved

---
+++ 06 Strings and Text Processing/10_FormatNumber/FormatNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_FormatNumber
+{
+    class FormatNumber
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("{0,-13}", "Decimal:");
+            Console.WriteLine("{0,15}", number);
+            Console.Write("{0,-13}", "Hexadecimal:");
+            Console.WriteLine("{0,15:X}", number);
+            Console.Write("{0,-13}", "Percentage:");
+            Console.WriteLine("{0,15:P02}", number / double.Parse('1' + new string('0', number.ToString().Length)));
+            Console.Write("{0,-13}", "Scientific:");
+            Console.WriteLine("{0,15:E}", number);
+        }
+    }
+}
