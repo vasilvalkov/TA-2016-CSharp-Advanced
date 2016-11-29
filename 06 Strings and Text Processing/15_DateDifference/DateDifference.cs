3cf2b7241dec0dfc43c470848029f7c77664f916
vasilvalkov <vasil.valkov@gmail.com>
2016-11-29 18:10:13 +0200

    Date Difference - solved

---
+++ 06 Strings and Text Processing/15_DateDifference/DateDifference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace _15_DateDifference
+{
+    class DateDifference
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first date: ");
+            DateTime firstDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Enter the second date: ");
+            DateTime secondDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Distance: ");
+            Console.WriteLine("{0} days", CalculateDifference(firstDate, secondDate));
+        }
+
+        private static int CalculateDifference(DateTime firstDate, DateTime secondDate)
+        {
+            DateTime greaterDate = firstDate > secondDate ? firstDate : secondDate;
+            DateTime smallerDate = firstDate < secondDate ? firstDate : secondDate;
+
+            TimeSpan diff = greaterDate - smallerDate;
+
+            return (int)(diff.TotalDays);
+        }
+    }
+}
