36d417415721c671ef85cdb61674199b6731c53c
vasilvalkov <vasil.valkov@gmail.com>
2016-11-29 19:03:33 +0200

    Date In Bulgarian - solved

---
+++ 06 Strings and Text Processing/16_DateInBulgarian/DateInBulgarian.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace _16_DateInBulgarian
+{
+    class DateInBulgarian
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("bg-BG");
+
+            DateTime inputDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy HH:mm:ss", CultureInfo.CurrentCulture);
+
+            DateTime amendedDate = inputDate.AddHours(6).AddMinutes(30);
+
+            Console.WriteLine("{0:dddd dd.MM.yyyy HH:mm:ss}", amendedDate);
+        }
+    }
+}
