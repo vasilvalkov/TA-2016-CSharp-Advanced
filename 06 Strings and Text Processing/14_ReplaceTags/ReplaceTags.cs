0106b041d9880903713e4fcd3d198616a20c338d
vasilvalkov <vasil.valkov@gmail.com>
2016-12-01 20:02:02 +0200

    Replace Tags - solved

--- 06 Strings and Text Processing/14_ReplaceTags/ReplaceTags.cs
+++ 06 Strings and Text Processing/14_ReplaceTags/ReplaceTags.cs
@@ -8,38 +8,45 @@ namespace _14_ReplaceTags
         static void Main()
         {
             string input = Console.ReadLine();
-
-            Console.WriteLine(MarkdownParse(input));
+            string parsedInput = MarkdownParse(input);
+            Console.WriteLine(parsedInput);
         }
 
-        private static string MarkdownParse(string input)
+        private static string MarkdownParse(string text)
         {
             //string parsedHTML = Regex.Replace(text, "(<a href=\")(.*?)(\">)(.*?)(</a>)", "[$4]($2)");
             StringBuilder parsedText = new StringBuilder();
-            parsedText.Append(input);
             string aTagOpen = "<a href=\"";
             string aTagClose = "</a>";
             string urlTagClose = "\">";
+            int startIndex = 0;
 
-            int startAnchor = input.IndexOf(aTagOpen);
-            int endAnchor = input.IndexOf(aTagClose);
-            int endUrl = input.IndexOf(urlTagClose);
-
-            while (endUrl > startAnchor + aTagOpen.Length)
+            while (true)
             {
-                if (input.IndexOf(aTagOpen) < 0 ||
-                    input.IndexOf(urlTagClose) < 0 ||
-                    input.IndexOf(aTagClose) < 0)
+                int startAnchor = text.IndexOf(aTagOpen, startIndex);
+                if (startAnchor < 0)
                     break;
-
-                string url = url = input.Substring(startAnchor + aTagOpen.Length, endUrl - (startAnchor + aTagOpen.Length));
-                string text = input.Substring(endUrl + urlTagClose.Length, endAnchor - (endUrl + urlTagClose.Length));
-
-                parsedText.Remove(startAnchor, (endAnchor + aTagClose.Length) - startAnchor);
-                parsedText.Insert(startAnchor, string.Format("[{0}]({1})", text, url));
-
-                input = parsedText.ToString();
+                // Append text up to the openning anchor tag
+                parsedText.Append(text, startIndex, startAnchor - startIndex);
+                // Update position in text
+                startIndex = startAnchor + aTagOpen.Length;
+                // Get the url
+                int endUrl = text.IndexOf(urlTagClose, startIndex);
+                string url = text.Substring(startIndex, endUrl - startIndex);
+                int endAnchor = text.IndexOf(aTagClose, startIndex);
+                // update position in text
+                startIndex = endUrl + urlTagClose.Length;
+                // Append anchor text and url in md format
+                parsedText.Append("[");
+                parsedText.Append(text, startIndex, endAnchor - startIndex);
+                parsedText.Append("](");
+                parsedText.Append(url);
+                parsedText.Append(")");
+                // Update position in text
+                startIndex = endAnchor + aTagClose.Length;
             }
+
+            parsedText.Append(text, startIndex, text.Length - startIndex);
 
             return parsedText.ToString();
         }
