4535281e2143d826eb05593a8394297a612cbdc4
vasilvalkov <vasil.valkov@gmail.com>
2016-12-02 14:51:12 +0200

    Enter Numbers - solved

---
+++ 07 Exception Handling/02_EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02_EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void ReadNumber(int start, int end)
+        {
+            string output = "1";
+            int compareNumber = start;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (compareNumber < number && number < end)
+                    {
+                        output += string.Format(" < {0}", number);
+                        compareNumber = number;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Exception");
+                    return;
+                }
+            }
+
+            Console.WriteLine(output + " < 100");
+        }
+
+        static void Main()
+        {
+            ReadNumber(1, 100);
+        }
+    }
+}
