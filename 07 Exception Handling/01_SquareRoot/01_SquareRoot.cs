5bf952a739f6e3ca03fc05117abe7d6e4ea890a4
vasilvalkov <vasil.valkov@gmail.com>
2016-12-02 14:04:17 +0200

    Square Root - solved

---
+++ 07 Exception Handling/01_SquareRoot/01_SquareRoot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_SquareRoot
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("{0:F3}", Math.Sqrt(input));
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ 07 Exception Handling/README.md
@@ -0,0 +1,9 @@
+Homework: Exception Handling
+============================
+
+### Problem List
+
+1. [Square root](./01_SquareRoot)
+1. [Enter numbers](./02_EnterNumbers)
+1. [Read file contents](./03_ReadFileContents)
+1. [Download file](./04_DownloadFile)
