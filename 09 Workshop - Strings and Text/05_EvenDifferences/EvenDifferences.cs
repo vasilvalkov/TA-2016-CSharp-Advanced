e0f61ae3e7d395d0a6443cec5be14a0f15f34774
vasilvalkov <vasil.valkov@gmail.com>
2016-12-04 18:48:08 +0200

    Even Difference - solved

---
+++ 09 Workshop - Strings and Text/05_EvenDifferences/EvenDifferences.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_EvenDifferences
+{
+    class EvenDifferences
+    {
+        static long sum = 0;
+        static void Main()
+        {
+            long[] numbers = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+            EvenSum(numbers);
+
+            Console.WriteLine(sum);
+        }
+        private static void EvenSum(long[] numbers)
+        {
+            for (int i = 1; i < numbers.Length;)
+            {
+                long diff = Math.Abs(numbers[i] - numbers[i - 1]);
+
+                if (diff % 2 == 0)
+                {
+                    sum += diff;
+                    i += 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
