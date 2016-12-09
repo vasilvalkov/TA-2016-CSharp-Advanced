e2d40e4e75f253abc5504f6cf3e4553efe944dcc
vasilvalkov <vasil.valkov@gmail.com>
2016-12-09 14:16:36 +0200

    Odd Lines - solved

---
+++ 08 Working With Text File/01_OddLines/OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _01_OddLines
+{
+    class OddLines
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            using (StreamReader reader = new StreamReader("../../TestFile.txt", Encoding.UTF8))
+            {
+                string line = reader.ReadLine();
+                int i = 1;
+
+                while (line != null)
+                {
+                    if (i % 2 == 0)
+                    {
+                        i++;
+                        continue;
+                    }
+                    Console.WriteLine("Line {0}: {1}", i++, line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
