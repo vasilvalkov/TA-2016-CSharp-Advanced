0a5b3654ec05360aebdf191f802a3b914ad1fa38
vasilvalkov <vasil.valkov@gmail.com>
2016-12-09 13:09:05 +0200

    Download File - solved

---
+++ 07 Exception Handling/04_DownloadFile/DownloadFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace _04_DownloadFile
+{
+    class DownloadFile
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string fileName = path.Substring(path.LastIndexOf('/') + 1);
+
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    client.DownloadFile(path, fileName);
+                }
+                catch
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+        }
+    }
+}
