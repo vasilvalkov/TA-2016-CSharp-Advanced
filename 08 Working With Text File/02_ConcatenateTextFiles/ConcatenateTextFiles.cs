f90c8061ea06215bf881b73b620099db27cb5d1a
vasilvalkov <vasil.valkov@gmail.com>
2016-12-09 16:43:15 +0200

    refactor

--- 08 Working With Text File/02_ConcatenateTextFiles/ConcatenateTextFiles.cs
+++ 08 Working With Text File/02_ConcatenateTextFiles/ConcatenateTextFiles.cs
@@ -8,14 +8,14 @@ namespace _02_ConcatenateTextFiles
     {
         static void Main()
         {
-            string file1 = Console.ReadLine().Trim('"');
-            string file2 = Console.ReadLine().Trim('"');
+            string file1path = Console.ReadLine().Trim('"');
+            string file2path = Console.ReadLine().Trim('"');
 
             using (var writer = new StreamWriter("../../Concatenated.txt", true, Encoding.UTF8))
             {
                 writer.AutoFlush = true;
 
-                using (var reader1 = new StreamReader(file1, Encoding.UTF8))
+                using (var reader1 = new StreamReader(file1path, Encoding.UTF8))
                 {
                     while (reader1.EndOfStream == false)
                     {
@@ -23,7 +23,7 @@ namespace _02_ConcatenateTextFiles
                     } 
                 }
                 
-                using (var reader2 = new StreamReader(file2, Encoding.UTF8))
+                using (var reader2 = new StreamReader(file2path, Encoding.UTF8))
                 {
                     while (reader2.EndOfStream == false)
                     {
