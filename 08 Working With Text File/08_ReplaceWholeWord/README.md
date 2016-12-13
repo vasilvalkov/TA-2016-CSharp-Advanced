4e749d52eef4b7a0e37d1102a00051d6f050eb09
vasilvalkov <vasil.valkov@gmail.com>
2016-12-13 12:06:12 +0200

    Replace Whole Word - solved

---
+++ 08 Working With Text File/08_ReplaceWholeWord/README.md
@@ -0,0 +1,4 @@
+﻿# Replace whole word
+
+## Description
+Modify the solution of the previous problem to replace only **whole words** (not strings).
---
+++ 08 Working With Text File/08_ReplaceWholeWord/ReplaceWholeWord.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08_ReplaceWholeWord
+{
+    class ReplaceWholeWord
+    {
+        static void Main()
+        {
+            string inputFilePath = "../../input.txt";
+            string outputFilePath = "../../output.txt";
+            bool append = false;
+            Encoding enc = Encoding.UTF8;
+
+            using (StreamReader reader = new StreamReader(inputFilePath, enc))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath, append, enc))
+                {
+                    try
+                    {
+                        writer.AutoFlush = true;
+                        Regex regexLower = new Regex(@"(?=start)\b\w+");
+                        Regex regexUpper = new Regex(@"(?=Start)\b\w+");
+
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            if (regexLower.IsMatch(line))
+                            {
+                                line = regexLower.Replace(line, "finish");
+                            }
+                            if (regexUpper.IsMatch(line))
+                            {
+                                line = regexUpper.Replace(line, "Finish");
+                            }
+
+                            writer.WriteLine(line);
+                        }
+                    }
+                    finally
+                    {
+                        writer.Close();
+                        reader.Close();
+                    }
+                }
+            }
+        }
+    }
+}
