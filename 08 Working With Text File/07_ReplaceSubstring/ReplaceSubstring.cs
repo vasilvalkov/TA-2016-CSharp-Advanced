2e631d6a915f9fb56f5c7ec2d94e4fac9d7aaf18
vasilvalkov <vasil.valkov@gmail.com>
2016-12-13 10:17:45 +0200

    Replace Sub-string - solved

---
+++ 08 Working With Text File/07_ReplaceSubstring/ReplaceSubstring.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text;
+
+namespace _07_ReplaceSubstring
+{
+    class ReplaceSubstring
+    {
+        static void Main()
+        {
+            string inputFilePath = "../../input.txt";
+            string outputFilePath = "../../output.txt";
+            bool append = false;
+            Encoding enc = Encoding.UTF8;
+
+            using (StreamReader reader = new StreamReader(inputFilePath, enc))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath, append, enc))
+                {
+                    try
+                    {
+                        writer.AutoFlush = true;
+
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            writer.WriteLine(line.Replace("start", "finish"));
+                        }
+                    }
+                    finally
+                    {
+                        writer.Close();
+                        reader.Close();
+                    }
+                }
+            }
+        }
+    }
+}
